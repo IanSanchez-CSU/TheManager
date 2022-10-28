83240d533d453efea946c93e32fb2e765fb9c4df
IanSanchez-CSU <116918709+IanSanchez-CSU@users.noreply.github.com>
2022-10-28 13:35:21 -0700

    second commit

---
+++ StarterPlayer/StarterPlayerScripts/Client/Driver.lua
@@ -0,0 +1,16 @@
+local client = require(script.Parent)
+client.Util = require(script.Parent:WaitForChild("Util"))
+client.Menus = require(script.Parent:WaitForChild("Menus"))
+
+--Declarations
+local MainStep = require(script.Parent:WaitForChild("Main"))
+
+--Services
+local RunService = game:GetService("RunService")
+
+--Main Loop
+game:GetService("StarterGui"):SetCoreGuiEnabled(Enum.CoreGuiType.All, false)
+while true do
+	local dt = RunService.RenderStepped:Wait()
+	MainStep(dt)
+end
